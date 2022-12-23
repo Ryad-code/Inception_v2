75dca77b2a47d26c322f28b42f7862c4ba310708
ryad <ryad.laouedj@gmail.com>
2022-12-23 14:15:35 +0100

    Inception_v2_23/12/2022

--- requirements/nginx/conf/ssl.sh
+++
@@ -1,4 +0,0 @@
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
--out /etc/nginx/ssl/mlaouedj.pem \
--keyout /etc/nginx/ssl/mlaouedj.key \
--subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=mlaouedj/CN=mlaouedj/"
